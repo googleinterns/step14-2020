9189ebf2baffbc6c6a4d2e2dd03223f95f063f0f
arrington <arrington@brown.edu>
2020-06-23 21:29:59 +0000

    Update datastore with inputted message.

--- public/script.js
+++ public/script.js
@@ -2,14 +2,24 @@ function init() {
     test();
 }
 
+const path = '/messages'; // can make this more detailed
+
 function test() {
     const divObject = document.getElementById('content');
 
     // create database reference
-    const dbRefObject = firebase.database().ref().child('object');
+    const dbRefObject = firebase.database().ref(path);
 
     // sync object data
     dbRefObject.on('value', snap => {
         divObject.innerHTML = JSON.stringify(snap.val(), null, 3);
     });
+}
+
+function pushChatMessage() {
+    const messageRef = firebase.database().ref(path);
+
+    const message = document.getElementById('message-input').value;
+    // push message to datastore
+    messageRef.push(message);
 }
