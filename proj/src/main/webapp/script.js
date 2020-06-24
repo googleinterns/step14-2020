a1ea29fdb8cd7733034eebb60d8b6c4d54f5825e
arrington <arrington@brown.edu>
2020-06-24 18:55:37 +0000

    Move files to maven directory.

--- proj/src/main/webapp/script.js
+++ proj/src/main/webapp/script.js
@@ -1,17 +1,32 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+function init() {
+    initRef();
+}
 
-/**
- * Adds a random greeting to the page.
- */
+const path = '/messages'; // can make this more detailed (for example add user ID)
+
+// initializes the .on() functions for the database reference
+function initRef() {
+    // create database reference
+    const dbRefObject = firebase.database().ref(path);
+
+    // sync object data
+    const divObject = document.getElementById('content');
+    dbRefObject.on('value', snap => {
+        divObject.innerHTML = JSON.stringify(snap.val(), null, 3);
+    });
+
+    const listObject = document.getElementById('chat-as-list');
+    dbRefObject.on('child_added', snap => {
+        const li = document.createElement('li');
+        li.innerText = snap.val();
+        listObject.appendChild(li);
+    });
+}
+
+function pushChatMessage() {
+    const messageRef = firebase.database().ref(path);
+
+    const message = document.getElementById('message-input').value;
+    // push message to datastore
+    messageRef.push(message);
+} 
