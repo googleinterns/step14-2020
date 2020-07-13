e66ede8e77a55283fc2bd212ece2620c7a747f19
arrington <arrington@brown.edu>
2020-07-13 19:33:04 +0000

    Move auth to db.js; move location to location.js.

--- proj/src/main/webapp/db.js
+++ proj/src/main/webapp/db.js
@@ -1,4 +1,190 @@
 /*
+    Authentication
+ */
+
+// Elements of login container
+const fname = document.getElementById("fname")
+const txtEmail = document.getElementById("email");
+const txtPassword = document.getElementById("pass");
+const tagStr = document.getElementById("tags");
+const btnLogin = document.getElementById("btnLogin");
+const btnSignUp = document.getElementById("btnSignUp");
+const btnLogout = document.getElementById("btnLogout");
+
+// Add login event
+if(btnLogin){
+    btnLogin.addEventListener("click", e => {
+        const emailVal = txtEmail.value;
+        const passVal = pass.value;
+
+        // Initialize auth object
+        const auth = firebase.auth();
+
+        const promise = auth.signInWithEmailAndPassword(emailVal, passVal).then(function(user){
+            window.location.replace("chat.html");
+        });
+        promise.catch(e => console.log(e.message));
+    });
+}
+
+
+// Adds user to an existing chat when given a reference to the place in the database
+function addUserToTag(reference){
+    var currentUID = firebase.auth().currentUser.uid;
+    console.log("adding new user to chat room with uid: " + currentUID);
+    reference.push(currentUID);
+}
+
+// Creates a new chat given a tag and adds the current user as a member
+function createNewChatWithUser(tag){
+    console.log("creating new chat with tag: " + tag);
+    var time = new Date().getTime();
+    var messageContent = "Welcome to the " + tag + " chat!";
+    var newChat = {
+        "chatInfo" : {
+            "name" : tag,
+            "tag" : tag,
+            "lastMessage" : messageContent,
+            "timestamp" : time,
+            "lastAuthor" : ""
+        },
+        "messages" : {
+            "welcome" : {
+                "content" : messageContent,
+                "timestamp" : time,
+                "senderDisplay" : "",
+                "senderUID" : "admin"
+            }
+        }
+    };
+    var currentReference = firebase.database().ref("/chat/" + tag);
+    var postKey = currentReference.push(newChat).key;
+    currentReference = firebase.database().ref("/chat/" + tag + "/" + postKey + "/users/");
+    addUserToTag(currentReference);
+
+    return postKey;
+}
+
+MAX_CHAT_SIZE = 200;
+
+// Loops through open chat rooms, adds the user to the first open chat and returns the key
+function findChatAndAddUser(snapshot){
+    var key;
+    var foundOpenRoom = false;
+    snapshot.forEach(function(childSnapshot){
+        key = childSnapshot.key;
+        console.log("key1 " + key);
+
+        var userSnapshot = childSnapshot.child("users");
+        var usersReference = userSnapshot.ref;
+
+        // Handled below if spillover is needed
+        if(userSnapshot.numChildren() < MAX_CHAT_SIZE){
+            addUserToTag(usersReference);
+            foundOpenRoom = true;
+            return true;
+        }
+    });
+    if(foundOpenRoom){
+        return key;
+    } else {
+        // Create new chat if room is full, add new user
+        return createNewChatWithUser(currentTag);
+    }
+}
+
+// Create or join chatroom
+function createOrJoinChat(currentTag){
+    var ref = firebase.database().ref("/chat/");
+    return ref.once("value").then(function(snapshot){
+        // Checks to see if tag already exists in database
+        if(snapshot.hasChild(currentTag)){
+
+            // If tag already exists, navigate to users and add users if there is room
+            var query = firebase.database().ref("/chat/" + currentTag + "/").orderByKey();
+            return query.once("value").then(function(snapshot){
+
+                //adds user to chat and returns key
+                return findChatAndAddUser(snapshot);
+            });
+        }
+        else{
+            // Create new chat if tag does not exist yet
+           return createNewChatWithUser(currentTag);
+        }
+    }).catch(function(err){
+        console.log("unexpected error searching for chat rooms:", err);
+    });
+}
+
+
+// Add sign up event
+if(btnSignUp){
+    btnSignUp.addEventListener("click", e => {
+        const emailVal = txtEmail.value;
+        const passVal = pass.value;
+        var tagList = tagStr.value.split(',');
+        for(var ii = 0; ii < tagList.length; ii++){
+            tagList[ii] = tagList[ii].trim();
+        }
+
+        // Initialize auth object
+        const auth = firebase.auth();
+        auth.useDeviceLanguage();
+
+        auth.createUserWithEmailAndPassword(emailVal, passVal).then(async function(){
+            var allTags = {};
+            for(var ii = 0; ii < tagList.length; ii++){
+
+                var tag = tagList[ii];
+                var key = await createOrJoinChat(tag)
+                allTags[tag] = key;
+            }
+
+            const user = auth.currentUser;
+            user.updateProfile({
+                displayName: fname.value + " " + lname.value
+                }).then(function(){
+                console.log("display name updated successfully");
+                firebase.database().ref("users/" + auth.currentUser.uid).set({
+                    firstName : fname.value,
+                    lastName : lname.value,
+                    allTags : allTags,
+                    bio : "I'm a new user! Say hi!"
+                }).then(function(){
+                    window.location.replace("chat.html");
+                });
+            }).catch(function(err){
+                console.log("error updating display name:", err);
+            });
+        });
+
+    });
+}
+
+if(btnLogout){
+    btnLogout.addEventListener("click", e => {
+        firebase.auth().signOut();
+        window.location.replace("welcome.html");
+        console.log("You logged out")
+    });
+}
+
+firebase.auth().onAuthStateChanged(firebaseUser => {
+    if(firebaseUser){
+        console.log(firebaseUser);
+        if(btnLogout)
+            btnLogout.classList.remove("hidden");
+    }
+    else{
+        console.log("not logged in");
+        if(btnLogout)
+            btnLogout.classList.add("hidden");
+    }
+});
+
+
+/*
     Realtime Database
  */
 
