4bfac802dcaacd0c74252eafc277e79d4033cb68
Nick Lumiere <nicholaslumiere@gmail.com>
2020-07-16 13:42:09 +0000

    admin init

--- proj/src/main/webapp/app.js
+++ proj/src/main/webapp/app.js
@@ -11,3 +11,13 @@ var firebaseConfig = {
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://arringtonh-step-2020-d.firebaseio.com"
+});
