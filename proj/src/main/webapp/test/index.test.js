e527ea826d19c60af47d0d70f8c0c20664282566
Arinze Okeke <aokeke@google.com>
2020-07-18 00:28:49 +0000

    erase comments

--- proj/src/main/webapp/test/index.test.js
+++ proj/src/main/webapp/test/index.test.js
@@ -13,7 +13,7 @@ const sampleFunctions = require("../sample.js");
 // const scriptJsFunctions = require("../script.js");
 // Test for tests
 before(async () => {  
-  process.env.FIREBASE_CONFIG = JSON.stringify(app.firebaseConfig) //'{"apiKey":"AIzaSyAcpxnpwrTCO4XTymTcneRscMBzJBne2Qg","authDomain":"arringtonh-step-2020-d.firebaseapp.com","databaseURL":"https://arringtonh-step-2020-d.firebaseio.com","projectId":"arringtonh-step-2020-d","storageBucket":"arringtonh-step-2020-d.appspot.com","messagingSenderId":"336825043126","appId":"1:336825043126:web:1256d7b08f1c8daa93be17","measurementId":"G-VJQS6TEYGV"}';
+  process.env.FIREBASE_CONFIG = JSON.stringify(app.firebaseConfig)
 })
 describe('uselessFunction', () => {
     it('please work', () => {
