9443075ef0bf6635f1b85b93562e3e55e3ec989f
Arinze Okeke <aokeke@google.com>
2020-07-24 17:47:53 +0000

    fix redirection when no page is given

--- proj/src/main/webapp/app.js
+++ proj/src/main/webapp/app.js
@@ -2,9 +2,9 @@
 const firebase = require('firebase');
 const appconfig = require('./appconfig.js');
 const { JSDOM } = require('jsdom');
-const { window } = new JSDOM('<!doctype html><html><body></body></html>');
-global.document = window.document;
+window = (new JSDOM('')).window;
 global.window = window;
+global.document = window.document;
 
 // Initialize Firebase
 firebase.initializeApp(appconfig.firebaseConfig);
@@ -20,11 +20,18 @@ function redirectToWelcomeOrChat() {
     const welcomePageUrl = '/static/welcome.html';
     const signupPageUrl = '/static/signup.html';
     const chatPageUrl = '/static/chat.html';
-    if(!firebase.auth().currentUser && !(window.location.href.endsWith(welcomePageUrl)||window.location.href.endsWith(signupPageUrl))) {
-        window.location.replace(welcomePageUrl);
-    } else {
-        window.location.replace(chatPageUrl);
-    }
+
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is already signed in.
+            window.location.replace(chatPageUrl);
+        } else {
+            // No user is signed in.
+            if (!(window.location.href.endsWith(welcomePageUrl)||window.location.href.endsWith(signupPageUrl))){
+                window.location.replace(welcomePageUrl);
+            }
+        }
+    });
 }
 
-window.redirectToWelcomeOrChat = redirectToWelcomeOrChat
+window.redirectToWelcomeOrChat = redirectToWelcomeOrChat;
