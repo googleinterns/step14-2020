0ecaaded5d4775b0ce4d36939c640382597ba129
Nicholas Lumiere <nicholaslumiere@gmail.com>
2020-08-07 09:49:02 -0700

    Update appconfig.js

--- proj/src/main/webapp/appconfig.js
+++ proj/src/main/webapp/appconfig.js
@@ -1,11 +1,4 @@
 exports.firebaseConfig = {
-    apiKey: "AIzaSyAcpxnpwrTCO4XTymTcneRscMBzJBne2Qg",
-    authDomain: "arringtonh-step-2020-d.firebaseapp.com",
-    databaseURL: "https://arringtonh-step-2020-d.firebaseio.com",
-    projectId: "arringtonh-step-2020-d",
-    storageBucket: "arringtonh-step-2020-d.appspot.com",
-    messagingSenderId: "336825043126",
-    appId: "1:336825043126:web:1256d7b08f1c8daa93be17",
-    measurementId: "G-VJQS6TEYGV"
+    
 };
-exports.serverKey = 'key=AAAATmxWSLY:APA91bFngBwWTr1GyOT_bxAK1aiCtj4wPc8QT7NvrAi1mwkYYxSQGUn7ZeHKarx_Oc0OH_7qe61VaWFdKybizER9G77xP9Y3f77R9l4t2095CWSCPx77xM7nprK6jWihdjlAarRb2Zd3';
+exports.serverKey = 'YOUR-SERVER-KEY';
